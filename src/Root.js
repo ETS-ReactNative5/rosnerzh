31ace2d80011b67df3481ca2298f1c5b79a154dd
DiDex <wc34fun@mail.ru>
2018-08-03 16:24:49 +0800

    add modules

---
+++ src/Root.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+import MenuSrc from './img/Menu.jpg';
+
+// Root component;
+class Root extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Root component</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default Root;
