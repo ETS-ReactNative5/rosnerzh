22b8dbbe3ffeff1f043986eec24d722b7a34b5fc
DiDex <wc34fun@mail.ru>
2018-08-04 15:08:48 +0800

    humberger styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -8,5 +7,5 @@ if (module.hot) {
   module.hot.accept();
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('⚛'));
 registerServiceWorker();
