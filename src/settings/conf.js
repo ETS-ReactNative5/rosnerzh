86f7f669eee05287f1e8ddde53914b04ef2a2fab
DiDex <wc34fun@mail.ru>
2018-08-31 22:10:40 +0800

    get price

--- src/settings/conf.js
+++ src/settings/conf.js
@@ -1,11 +1,11 @@
 // colors hex
 export const colors = [
+  {color: '#b3b3b3'},
   {color: '#448ed4'},
   {color: '#c7ac7e'},
   {color: '#006e69'},
   {color: '#82c09a'},
   {color: '#344099'},
-  {color: '#b3b3b3'},
 ]
 // react motion presents
 export const transformPreset = {stiffness: 120, damping: 14}
--- src/store/Constructor.js
+++ src/store/Constructor.js
@@ -10,7 +10,7 @@ class Contructour {
   @observable gateLength = 400
   @observable type = "ladder"       // ladder | mType | pType | fType | gType
   @observable form = 0
-  @observable color = 5
+  @observable color = 0
   @observable energy = false
   @observable rail = false
   @observable gate = false
@@ -30,7 +30,7 @@ class Contructour {
   }
   @action('set-color')
   setColor = value => {
-    if(value === this.color) return this.color = 5
+    if(value === this.color) return this.color = 0
     this.color = value
   }
   @action('set-rail')
@@ -71,12 +71,19 @@ class Contructour {
     if(this.rack) res += " rack"
     return `${res} ${this.color}`
   }
-  //TODO: fix currency format, and round +/- 50 units
   @computed
   get price() {
     let price = data.type[this.type]
-    const formatter = new Intl.NumberFormat('ru', 'currency');
+    price *= data.rail[+this.rail]
+    price *= data.width[+this.width]
+    price *= data.height[+this.height]
+    if(this.rack) price * 0.2 < 300 ? price += 300 : price *= 1.2;
+    price += data.form[this.form]
+    price += data.energy[+this.energy]
+    price += data.gate[+this.gate]
+    if(this.color) price += data.color
 
+    const formatter = new Intl.NumberFormat('ru', 'currency');
     return formatter.format(Math.round(price / 50) * 50)
   }
 }
@@ -124,12 +131,14 @@ const data = {
       "6" : 0,		//	прямая
       "7" : 50,		//	трапеция
       "8" : 2300},	//	NEO 2
-  "base"  : {				//	Цены на тип сушилки
-      "1" : 0,		//	Водянной
-      "2" : 3100},	//	Электрический
-  "jumper": 1.13,			//	Цена на группировку перемычек
-  "hook"  : {				//	Цена для варианта подключения
-      "1" : 0,		//	Универсальное 
-      "2" : 1000},	//	Боковое 110%
+  "energy": {				//	Цены на тип сушилки
+      "0" : 0,		//	Водянной
+      "1" : 3100},	//	Электрический
+  "rail": {
+      "0": 1,
+      "1": 1.13},			//	Цена на группировку перемычек
+  "gate"  : {				//	Цена для варианта подключения
+      "0" : 0,		//	Универсальное 
+      "1" : 1000},	//	Боковое 110%
   "color" : 4000,			//	Цена за перекраску
   }
