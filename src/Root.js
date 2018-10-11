cf0693bcc1eafe319f21350859ff5ad0ecfa3d02
DiDex <wc34fun@mail.ru>
2018-10-11 17:23:38 +0800

    code spliting

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react'
 import {inject, observer} from 'mobx-react'
+import Loadable from 'react-loadable'
 
 import Header from './components/header'
 import Preview from './components/preview'
@@ -7,9 +8,13 @@ import MenuButton from './components/MenuButton'
 import Benefits from './components/Benefits'
 import Grid from './components/Gird'
 import Footer from './components/footer'
-import Menu from './components/menu'
 // import 'antd/dist/antd.css'
 import './css/index.css'
+
+const LoadableMenu = Loadable({
+  loader: () => import('./components/menu'),
+  loading: () => <span></span>,
+})
 
 // Root component;
 @inject('menuStore')
@@ -22,15 +27,17 @@ class Root extends Component {
     return (
       <Fragment>
         <MenuButton />
-        <Menu />
+        <LoadableMenu />
         <main
           className="blur-container"
           style={{
-            filter: isOpen ? 'blur(5px) contrast(.7) brightness(.8)' : 'blur(0) contrast(1) brightness(1)',
+            filter: isOpen
+              ? 'blur(5px) contrast(.7) brightness(.8)'
+              : 'blur(0) contrast(1) brightness(1)',
             boxShadow: '0 0 0 50vmax rgba(0,0,0,.7)',
           }}
         >
-          <Header/>
+          <Header />
           <Preview />
           <Benefits />
           <Grid />
